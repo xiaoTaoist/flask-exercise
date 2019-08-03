f35369e2c86e0c28ace2ff958017dfa21042b2a2
xiaotaoist <taoist@protonmail.com>
2019-08-03 21:54:20 +0800

    myblog edit models

---
+++ myblog/myblog/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from myblog.extensions import db
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(24))
+    password_hash = db.Column(db.String(128))
+    blog_title = db.Column(db.String(48))
+    blog_sub_title = db.Column(db.String(64))
+    name = db.Column(db.String(32))
+    about = db.Column(db.Text)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+    posts = db.relationship('Category', back_populates='posts')
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    timtstamp = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', back_populates='posts')
+    comments = db.relationship('Comment', backref='post', cascade='all')
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(24))
+    email = db.Column(db.String(255))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False)
+    reviewed = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    replies = db.relationship('Comment', back_populates='replied', cascade='all')
+    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
