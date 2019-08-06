537764990204c4d873dc5c540afafbda62939376
xiaotaoist <taoist@protonmail.com>
2019-08-06 22:09:16 +0800

    myblog add fake data cmd

--- myblog/myblog/__init__.py
+++ myblog/myblog/__init__.py
@@ -76,3 +76,30 @@ def register_commands(app):
             click.echo('Drop tables.')
         db.create_all()
         click.echo('Initialized database.')
+
+    @app.cli.command()
+    @click.option('--category', default=10, help='Quantity of categories, default is 10.')
+    @click.option('--post', default=50, help='Quantity of posts, default is 50.')
+    @click.option('--comment', default=200, help='Quantity of comments, default is 200')
+    def forge(category, post, comment):
+        from myblog.fakes import fake_admin, faker_categoryies, fake_posts, fake_comments, fake_links
+
+        db.drop_all()
+        db.create_all()
+
+        click.echo('Generating the administrator...')
+        fake_admin()
+
+        click.echo('Generating {} categories...'.format(category))
+        faker_categoryies(category)
+
+        click.echo('Generating %d posts...' % post)
+        fake_posts(post)
+
+        click.echo('Generating %d comments...' % comment)
+        fake_comments(comment)
+
+        click.echo('Generating links...')
+        fake_links()
+
+        click.echo('Done.')
---
+++ myblog/myblog/fakes.py
@@ -0,0 +1,108 @@
+import random
+
+from faker import Faker
+from sqlalchemy.exc import IntegrityError
+
+from myblog.models import Admin, Category, Post, Comment, Link
+from myblog.extensions import db
+
+fake = Faker('zh-cn')
+
+
+def fake_admin():
+    admin = Admin(
+        username='admin',
+        blog_title='MyBlog',
+        blog_sub_title='test',
+        name='Taoist',
+        about='Hi...'
+    )
+    admin.set_password('admin123')
+    db.session.add(admin)
+    db.session.commit()
+
+
+def faker_categoryies(count=10):
+    category = Category(name='Default')
+    db.session.add(category)
+
+    for i in range(count):
+        category = Category(name=fake.word())
+        db.session.add(category)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
+def fake_posts(count=50):
+    for i in range(count):
+        post = Post(
+            title=fake.sentence(),
+            body=fake.text(1000),
+            category=Category.query.get(random.randint(1, Category.query.count())),
+            timtstamp=fake.date_time_this_year()
+        )
+
+        db.session.add(post)
+    db.session.commit()
+
+
+def fake_comments(count=200):
+    for i in range(count):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+        salt = int(count * 0.1)
+        for i in range(salt):
+            comment = Comment(
+                author=fake.name(),
+                email=fake.email(),
+                site=fake.url(),
+                body=fake.sentence(),
+                timestamp=fake.date_time_this_year(),
+                reviewed=False,
+                post=Post.query.get(random.randint(1, Post.query.count()))
+            )
+            db.session.add(Comment)
+
+        comment = Comment(
+            author='Taoist',
+            email='test@test.com',
+            site='test.me',
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            from_admin=True,
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+    for i in range(salt):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            replied=Comment.query.get(random.randint(1, Comment.query.count())),
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+
+def fake_links(count=5):
+    for i in range(count):
+        link = Link(name=fake.name(), url=fake.url)
+        db.session.add(link)
+    db.session.commit()
